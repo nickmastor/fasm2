b2e5ccad6c6e4031c694f03f4af0cfc57ea0bcd2
nickmastor <39601591+nickmastor@users.noreply.github.com>
2019-09-02 11:52:22 +0300

    code for read a text

---
+++ readtext/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace readtext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // The files used in this example are created in the topic
+            // How to: Write to a Text File. You can change the path and
+            // file name to substitute text files of your own.
+
+            // Example #1
+            // Read the file as one string.
+            string text = System.IO.File.ReadAllText(@"C:\Users\Νίκος\Desktop\12.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+            // Example #2
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Νίκος\Desktop\ultramark TOF_1.txt");
+
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of WriteLines2.txt1 = ");
+            
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+            string a = lines[0];
+            for (int i = 0; i < 4;  i++ )
+            {
+
+                Console.WriteLine(a.Substring(0, i));
+            };
+
+            int [] numbers = new int[] { 3, 14, 15, 92, 6 };
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} "+"nike\n");
+            }
+            Console.WriteLine();
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+           
+            System.Console.ReadKey();
+
+        }
+    }
+    
+};
